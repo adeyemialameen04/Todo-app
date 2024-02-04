0d554c98249ceec71139093cc8ec2a9e25a88b6e
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-02-04 14:53:53 +0100

    Trying to complete the todo app

--- new.js
+++ new.js
@@ -1,3 +1,77 @@
+const steps = [
+  {
+    id: 0,
+    title: "Customize your online store",
+    desc: "Choose a theme and add your logo, colors, and images to reflect your brand",
+    btnTxt: "Customize theme",
+    img: "https://crushingit.tech/hackathon-assets/customise-store.png",
+    learnMoreLink:
+      "https://help.shopify.com/en/manual/online-store/themes/customizing-themes",
+    ariaLabel:
+      "Step 1: Customize your online store - Choose a theme and add your logo, colors, and images to reflect your brand. Click the 'Customize theme' button to get started.",
+  },
+  {
+    id: 1,
+    title: "Add your first product",
+    desc: "Write a description, add photos, and set pricing for the products you plan to sell",
+    btnTxt: "Add product",
+    img: "https://crushingit.tech/hackathon-assets/product.png",
+    learnMoreLink:
+      "https://help.shopify.com/en/manual/online-store/themes/customizing-themes",
+    ariaLabel:
+      "Step 2: Add your first product - Write a description, add photos, and set pricing for the products you plan to sell. Click the 'Add product' button to get started.",
+  },
+  {
+    id: 2,
+    title: "Add a custom domain",
+    desc: "Your current domain is 222219.myshopify.com but you can add a custom domain to help customers find your online store",
+    btnTxt: "Add domain",
+    img: "https://crushingit.tech/hackathon-assets/website.png",
+    learnMoreLink:
+      "https://help.shopify.com/en/manual/online-store/themes/customizing-themes",
+    ariaLabel:
+      "Step 3: Add a custom domain - Your current domain is 222219.myshopify.com, but you can add a custom domain to help customers find your online store. Click the 'Add domain' button to get started.",
+  },
+  {
+    id: 3,
+    title: "Name your store",
+    desc: "Your temporary store name is currently Davii collections. The store name appears in your admin and your online store. Learn more",
+    btnTxt: "Name store",
+    img: "https://crushingit.tech/hackathon-assets/name-store.png",
+    learnMoreLink:
+      "https://help.shopify.com/en/manual/online-store/themes/customizing-themes",
+    ariaLabel:
+      "Step 4: Name your store - Your temporary store name is currently Davii collections. The store name appears in your admin and your online store. Click the 'Name store' button to get started.",
+  },
+  {
+    id: 4,
+    title: "Set up a payment provider",
+    desc: "Choose a payment provider to start accepting payments. You’ll need to create an account with the payment provider and set it up with your Shopify store",
+    btnTxt: "Set up payment",
+    img: "https://crushingit.tech/hackathon-assets/payment.png",
+    learnMoreLink:
+      "https://help.shopify.com/en/manual/online-store/themes/customizing-themes",
+    ariaLabel:
+      "Step 5: Set up a payment provider - Choose a payment provider to start accepting payments. You’ll need to create an account with the payment provider and set it up with your Shopify store. Click the 'Set up payment' button to get started.",
+  },
+];
+
+let todos = [
+  {
+    title: "This is a title",
+    content: "Na the content be this",
+    date: "2024-02-26",
+  },
+];
+const titleInput = document.getElementById("title");
+const dateInput = document.getElementById("date");
+const contentInput = document.getElementById("content");
+const addBtn = document.getElementById("addBtn");
+
+let title = titleInput.value;
+let contentTodo = contentInput.value;
+let date = dateInput.value;
+
 const searchInput = document.getElementById("search");
 const contentContainer = document.getElementById("content-container");
 const notificationButton = document.getElementById("notification-button");
@@ -5,8 +79,6 @@ const notificationContainer = document.getElementById("notification-container");
 const notificationContainerWrapper = document.getElementById(
   "notification-container-wrapper"
 );
-const closeBtn = document.querySelector(".close");
-const selectSection = document.getElementById("select-section");
 const stepsCardContainer = document.getElementById("steps-card-container");
 const stepsCardWrapper = document.getElementById("steps-card-wrapper");
 const arrowBtn = document.getElementById("arrow-btn");
@@ -81,97 +153,107 @@ window.addEventListener("resize", () => {
   updateHeights();
 });
 
-const stepsHtml = steps
-  .map(
-    (step, index) => `
-	  <li class="step-card" data-index="${index}">
+const updateTodoList = () => {
+  const stepsHtml = todos
+    .map(
+      (todo, index) => `
+  
+		<li class="step-card" data-index="${index}">
 		<div>
-		<span aria-live="polite" id="shopping-item-checkbox-status-${index}"></span>
-		  <button  role="menuitem"  aria-label="Mark ${step.title} as done" id="shopping-item-checkbox-${index}" class="shopping-item-checkbox">
-		  <svg
-			width="30"
-			height="30"
-			viewBox="0 0 32 32"
-			fill="none"
-			aria-hidden="true"
-			xmlns="http://www.w3.org/2000/svg"
-			class="not-completed-icon"
-			id="not-completed-icon-${index}"
+		  <span
+			aria-live="polite"
+			id="shopping-item-checkbox-status-${index}"
+		  ></span>
+		  <button
+			role="menuitem"
+			aria-label="Mark ${todo.title} as done"
+			id="shopping-item-checkbox-${index}"
+			class="shopping-item-checkbox"
 		  >
-			<circle
-			  cx="16"
-			  cy="16"
-			  r="10"
-			  stroke="#8A8A8A"
-			  stroke-width="2.5"
-			  stroke-linecap="round"
-			  stroke-linejoin="round"
-			  stroke-dasharray="4 6"
-			/>
-		  </svg>
-		  <svg
-			width="30"
-			height="30"
-			viewBox="0 0 24 24"
-			fill="none"
-			aria-hidden="true"
-			xmlns="http://www.w3.org/2000/svg"
-			class="loading-spinner-icon hidden"
-			id="loading-spinner-icon-${index}"
-		  >
-			<path
-			  d="M22.0004 12C22.0004 13.9778 21.4139 15.9112 20.3151 17.5557C19.2162 19.2002 17.6545 20.4819 15.8272 21.2388C13.9999 21.9957 11.9893 22.1937 10.0495 21.8079C8.10965 21.422 6.32782 20.4696 4.9293 19.0711C3.53077 17.6725 2.57837 15.8907 2.19251 13.9509C1.80666 12.0111 2.00469 10.0004 2.76157 8.17317C3.51845 6.3459 4.80017 4.78412 6.44466 3.6853C8.08916 2.58649 10.0226 2 12.0004 2"
-			  stroke="#1C181D"
-			  stroke-width="2.08333"
-			  stroke-linecap="round"
-			  stroke-linejoin="round"
-			/>
-		  </svg>
-		  <svg
-			width="30"
-			height="30"
-			viewBox="0 0 24 24"
-			fill="none"
-			aria-hidden="true"
-			xmlns="http://www.w3.org/2000/svg"
-			class="completed-icon hidden"
-			id="completed-icon-${index}"
-		  >
-			<circle cx="12" cy="12" r="10" fill="#303030"></circle>
-			<path
-			  d="M17.2738 8.52629C17.6643 8.91682 17.6643 9.54998 17.2738 9.94051L11.4405 15.7738C11.05 16.1644 10.4168 16.1644 10.0263 15.7738L7.3596 13.1072C6.96908 12.7166 6.96908 12.0835 7.3596 11.693C7.75013 11.3024 8.38329 11.3024 8.77382 11.693L10.7334 13.6525L15.8596 8.52629C16.2501 8.13577 16.8833 8.13577 17.2738 8.52629Z"
-			  fill="#fff"
-			></path>
-		  </svg>
-		</button>
-		  <button aria-label=${step.ariaLabel} class="step-title">
-			${step.title}
+			<svg
+			  width="30"
+			  height="30"
+			  viewBox="0 0 32 32"
+			  fill="none"
+			  aria-hidden="true"
+			  xmlns="http://www.w3.org/2000/svg"
+			  class="not-completed-icon"
+			  id="not-completed-icon-${index}"
+			>
+			  <circle
+				cx="16"
+				cy="16"
+				r="10"
+				stroke="#8A8A8A"
+				stroke-width="2.5"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-dasharray="4 6"
+			  />
+			</svg>
+			<svg
+			  width="30"
+			  height="30"
+			  viewBox="0 0 24 24"
+			  fill="none"
+			  aria-hidden="true"
+			  xmlns="http://www.w3.org/2000/svg"
+			  class="loading-spinner-icon hidden"
+			  id="loading-spinner-icon-${index}"
+			>
+			  <path
+				d="M22.0004 12C22.0004 13.9778 21.4139 15.9112 20.3151 17.5557C19.2162 19.2002 17.6545 20.4819 15.8272 21.2388C13.9999 21.9957 11.9893 22.1937 10.0495 21.8079C8.10965 21.422 6.32782 20.4696 4.9293 19.0711C3.53077 17.6725 2.57837 15.8907 2.19251 13.9509C1.80666 12.0111 2.00469 10.0004 2.76157 8.17317C3.51845 6.3459 4.80017 4.78412 6.44466 3.6853C8.08916 2.58649 10.0226 2 12.0004 2"
+				stroke="#1C181D"
+				stroke-width="2.08333"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			  />
+			</svg>
+			<svg
+			  width="30"
+			  height="30"
+			  viewBox="0 0 24 24"
+			  fill="none"
+			  aria-hidden="true"
+			  xmlns="http://www.w3.org/2000/svg"
+			  class="completed-icon hidden"
+			  id="completed-icon-${index}"
+			>
+			  <circle cx="12" cy="12" r="10" fill="#303030"></circle>
+			  <path
+				d="M17.2738 8.52629C17.6643 8.91682 17.6643 9.54998 17.2738 9.94051L11.4405 15.7738C11.05 16.1644 10.4168 16.1644 10.0263 15.7738L7.3596 13.1072C6.96908 12.7166 6.96908 12.0835 7.3596 11.693C7.75013 11.3024 8.38329 11.3024 8.77382 11.693L10.7334 13.6525L15.8596 8.52629C16.2501 8.13577 16.8833 8.13577 17.2738 8.52629Z"
+				fill="#fff"
+			  ></path>
+			</svg>
+		  </button>
+		  <button aria-label="${todo.content}" class="step-title">
+			${todo.title}
 		  </button>
 		</div>
 		<div class="step-details-wrapper" data-index="${index}">
 		  <div class="step-details">
 			<div>
-			  <p>
-				${step.desc}
-				<a href="${step.learnMoreLink}" class="learn-more-link">Learn more</a>
-			  </p>
-			  <button class="dark-btn">${step.btnTxt}</button>
-			</div>
-			<div class="step-img-container">
-			  <img
-				src="${step.img}"
-				alt=""
-				class="step-img"
-			  />
+			  <p>${todo.content}</p>
 			</div>
 		  </div>
 		</div>
 	  </li>
-	`
-  )
-  .join("");
+		`
+    )
+    .join("");
 
-stepsCardContainer.innerHTML = stepsHtml;
+  stepsCardContainer.innerHTML = stepsHtml;
+
+  // Add click event listener to each step title
+  const stepTitles = document.querySelectorAll(".step-title");
+  let openStepIndex = null;
+
+  stepTitles.forEach((title, index) => {
+    title.addEventListener("click", () => {
+      handleStep(index);
+    });
+  });
+};
 
 // Add click event listener to each step title
 const stepTitles = document.querySelectorAll(".step-title");
@@ -237,10 +319,15 @@ arrowBtn.addEventListener("click", () => {
         firstStepTitle.click();
       }
     }
-
-    checkBoxBtns.forEach((btn, index) => {
-      btn.addEventListener("keyup", (e) => {
-        handleMenuItemKeyPress(e, index, allMenuItems);
+    checkBoxBtns.forEach((button) => {
+      button.addEventListener("click", () => {
+        console.log("Checkbox clicked");
+        const index = parseInt(button.id.split("-").pop());
+        handleDoneOrNotDone(button);
+        setTimeout(() => {
+          stepCountTxt.innerText = `${markedStepsCount}`;
+          progressBar.style.width = `${progressWidth}px`;
+        }, 450);
       });
     });
 
@@ -341,10 +428,6 @@ document.addEventListener("click", (event) => {
   ) {
     closeAccordion(notificationContainerWrapper);
   }
-});
-
-closeBtn.addEventListener("click", () => {
-  selectSection.classList.add("hide");
 });
 
 // Event listener for user profile click
@@ -592,4 +675,40 @@ document.addEventListener("DOMContentLoaded", () => {
       }, 450);
     });
   });
+});
+
+const addTodo = () => {
+  if (title.trim() === "" || contentTodo.trim() === "") {
+    alert("You cant submit an empty todo");
+    return;
+  } else if (date.trim() === "") {
+    alert("Pls add a deadline 😉🥹");
+  }
+  const currentDate = new Date().toISOString().split("T")[0];
+  if (date < currentDate) {
+    alert("You can't select a date from the past 😒😒");
+    setDate("");
+    return;
+  }
+
+  const newTodo = {
+    title: title,
+    content: contentTodo,
+    date: date,
+  };
+
+  todos.unshift(newTodo);
+
+  updateTodoList();
+
+  console.log(todos);
+
+  alert(
+    "Todo created successfully. Go to the todos page to see your saved todos"
+  );
+};
+
+addBtn.addEventListener("click", () => {
+  addTodo();
+  console.log("How far na");
 });
